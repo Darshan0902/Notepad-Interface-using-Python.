63faa8a2913e78f0186a0b3d855d73269c1cde97
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-05-01 18:30:17 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,15 @@
+import pygame , sys , os , math
+from pygame.locals import*
 
+## Displaying the constants ##
+
+DISPLAY_WIDTH = 760
+DISPLAY_HEIGHT = 500
+FPS = 30
+TEXT_HEIGHT=30
+STARTY = 0
+STARTX = 0
+UP = "up"
+DOWN = "down"
+RIGHT = "right"
+LEFT = "left"
