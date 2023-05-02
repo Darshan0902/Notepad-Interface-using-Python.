4d598740036cf69557c886a68389152fe2b99f8b
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-05-03 00:15:26 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -13,3 +13,22 @@ UP = "up"
 DOWN = "down"
 RIGHT = "right"
 LEFT = "left"
+
+## COLORS ##
+
+#            R    G    B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
+BLACK    = (  0,   0,   0)
+COMBLUE  = (233, 232, 255)
+
+BGCOLOR = WHITE
+TEXTCOLOR = BLACK
